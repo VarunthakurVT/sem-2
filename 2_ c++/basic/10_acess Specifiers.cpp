679e01a99d46a4e6c34679ecbd8b02339cc743f7
Varun Thakur <vtthakurvarun@gmail.com>
2026-02-24 10:01:13 +0530

    learning the linked list

--- 2_ c++/basic/10_acess Specifiers.cpp
+++ 2_ c++/basic/10_acess Specifiers.cpp
@@ -3,4 +3,4 @@
 //public -- can be used in the class and outside 
 //protected--with in class and Inherited class;
 //scope resolution operator :: (Counter::Counter Operator++()){
-                                // count++}
+                                // count++}`
---
+++ 2_ c++/basic/question_sirgive/9_give the result.cpp
@@ -0,0 +1,10 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    for(i=0;i<10;i++)
+    if(i%5==0)
+    exit(1);
+    cout<<i;
+}
