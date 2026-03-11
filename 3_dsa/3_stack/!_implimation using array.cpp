3b2740b484caa009334be2696737d93f1d001498
varun thakur <vtthakurvarun@gmail.com>
2026-03-11 19:40:41 +0530

    arranging the class room file and also start stack

---
+++ 3_dsa/3_stack/!_implimation using array.cpp
@@ -0,0 +1,13 @@
+//last in first out
+//implatimation using array
+//in push operation top of stack pointer  increment in first then  we insert an element 
+ //1) push will take input 
+ //2)it do not 
+// 3) increment the top and then store
+//pop operation 
+//4) in this time complicity is one 
+//  1)it do not take input;
+//  2)it will retrun a value
+//  3)first store then drecrement 
+//  4) in this time complicity order of complicity is one 
+;
---
+++ 3_dsa/3_stack/1_basic.cpp
@@ -0,0 +1,5 @@
+//one side is open is and another is closed
+//there is push or pop function to insert and delete the element form the stack 
+//in stack we insert elemnet one by one 
+//last in first out 
+//first in last out 
